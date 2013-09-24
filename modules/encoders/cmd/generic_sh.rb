166d95faace672ab67d17289869af6eccf1264b4
James Lee <egypt@metasploit.com>
2013-09-23 19:34:52 -0500

    Make generic_sh more generic
    Also adds some specs for it in a ghettotastic round about way by abusing
    specs for Util::EXE

--- modules/encoders/cmd/generic_sh.rb
+++ modules/encoders/cmd/generic_sh.rb
@@ -65,24 +65,17 @@ class Metasploit3 < Msf::Encoder
     cmd = 'perl -e '
     qot = ',-:.=+!@#$%^&'
 
-    # Find a quoting character to use
-    state.badchars.unpack('C*') { |c| qot.delete(c.chr) }
-
-    # Throw an error if we ran out of quotes
-    raise RuntimeError if qot.length == 0
-
-    sep = qot[0].chr
-
     # Convert spaces to IFS...
     if (state.badchars.include?(" "))
+      if state.badchars.match(/[${IFS}]/n)
+        raise RuntimeError
+      end
       cmd.gsub!(/\s/, '${IFS}')
     end
 
     # Can we use single quotes to enclose the command string?
     if (state.badchars.include?("'"))
 
-      # Without quotes, we also need backslash to be able to use parens
-      # so the shell doesn't try to interpreter them.
       if (state.badchars.match(/[()\\]/))
         # We don't have parens, quotes, or backslashes so we have to use
         # barewords on the commandline for the argument to the pack
@@ -90,22 +83,39 @@ class Metasploit3 < Msf::Encoder
         # would interpret, so $ and & become badchars.
         qot.delete("$")
         qot.delete("&")
-        raise RuntimeError if qot.length == 0
-        sep = qot[0].chr
-        cmd << "system -e pack -e qq#{sep}H#{hex.length}#{sep},qq#{sep}#{hex}#{sep}"
+
+        # Perl chains -e with newlines, but doesn't automatically add
+        # semicolons, so the following will result in the interpreter
+        # seeing a file like this:
+        #    system
+        #    pack
+        #    qq^H*^,qq^whatever^
+        # Since system and pack require arguments (rather than assuming
+        # $_ when no args are given like many other perl functions),
+        # this works out to do what we need.
+        cmd << "system -e pack -e #{perl_qq(state, qot, hex)}"
+        if state.badchars.include?(" ")
+          # We already tested above to make sure that these chars are ok
+          # if space isn't.
+          cmd.gsub!(" ", "${IFS}")
+        end
       else
-        cmd << "system\\(pack\\(qq#{sep}H\\*#{sep},qq#{sep}#{hex}#{sep}\\)\\)"
+        # Without quotes, we can use backslash to escape parens so the
+        # shell doesn't try to interpreter them.
+        cmd << "system\\(pack\\(#{perl_qq(state, qot, hex)})\\)"
       end
+
     else
-      if (state.badchars.match(/[()]/))
+      # Quotes are ok, but we still need parens or spaces
+      if (state.badchars.match(/[()]/n))
         if (state.badchars.include?(" "))
           # No spaces allowed, no paranthesis, give up...
           raise RuntimeError
         end
 
-        cmd << "'system pack qq#{sep}H*#{sep},qq#{sep}#{hex}#{sep}'"
+        cmd << "'system pack #{perl_qq(state, qot, hex)}'"
       else
-        cmd << "'system(pack(qq#{sep}H*#{sep},qq#{sep}#{hex}#{sep}))'"
+        cmd << "'system(pack(#{perl_qq(state, qot, hex)}))'"
       end
     end
 
@@ -132,7 +142,8 @@ class Metasploit3 < Msf::Encoder
       if (state.badchars.include?("`"))
         # Last ditch effort, dollar paren
         if (state.badchars.include?("$") or state.badchars.include?("("))
-          raise RuntimeError
+          # No shell stuff, try perl
+          return encode_block_perl(state, buf)
         else
           buf = "$(/bin/echo -ne #{hex})"
         end
@@ -149,6 +160,21 @@ class Metasploit3 < Msf::Encoder
     end
 
     return buf
+  end
+
+  def perl_qq(state, qot, hex)
+
+    # Find a quoting character to use
+    state.badchars.unpack('C*') { |c| qot.delete(c.chr) }
+
+    # Throw an error if we ran out of quotes
+    raise RuntimeError if qot.length == 0
+
+    sep = qot[0].chr
+    # Use an explicit length for the H specifier instead of just "H*"
+    # in case * is a badchar for the module, and for the case where this
+    # ends up unquoted so the shell doesn't try to expand a path.
+    "qq#{sep}H#{hex.length}#{sep},qq#{sep}#{hex}#{sep}"
   end
 
 end
